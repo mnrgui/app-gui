febae94f641bd14741a4d36ee44dfc241d19ce5d
Guilherme Augusto Florido da Silva <ben0067181@aluno.firjansenaisesi.com.br>
2026-06-30 21:45:06 -0300

    meu-app-gui

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,11 @@
+function Contato() {
+  return (
+    <div>
+      <h1>Contato</h1>
+      <p>Bem-vindo à nossa página de contato! Aqui você pode encontrar informações sobre como entrar em contato conosco.</p>
+      <p>21 9800029282</p>
+    </div>
+  );
+}
+
+export { Contato };
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,12 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre Nós</h1>
+      <p>Bem-vindo à nossa página de contato! Aqui você pode encontrar informações sobre como entrar em contato conosco.</p>
+      <p>Se você tiver alguma dúvida, sugestão ou comentário, não hesite em nos enviar uma mensagem. Estamos sempre prontos para ouvir nossos clientes e melhorar nossos serviços.</p>
+      <p>Você pode nos contatar através do formulário abaixo ou pelos nossos canais de atendimento disponíveis.</p>
+    </div>
+  );
+}
+
+export { Sobre };
